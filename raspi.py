78972b609d5becc604093c28eaf86e7977d7830d
matija <vodokotlic.vudukotlic@gmail.com>
2017-05-06 11:36:29 +0200

    initial

---
+++ raspi.py
@@ -0,0 +1,12 @@
+from gpiozero import RGBLED as rgb
+from uvloop.asyncio import sleep as uvsleep
+
+
+l1 = rgb(9,11,10)
+l2 = rgb(6,5,13)
+
+def light(r=0, g=0, b=0):
+    return((100-r)/100, (100-g)/100, (100-b)/100)
+
+
+l1.color = light(10,50,70)
