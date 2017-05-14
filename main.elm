6a13560dd0793de2c8c80b794f8e9c7d98da0675
matija <vodokotlic.vudukotlic@gmail.com>
2017-05-14 22:13:35 +0200

    stepper micropython

--- main.elm
+++ main.elm
@@ -1,14 +1,55 @@
+import Html exposing (Html, div, input, label, button, text)
+
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
+import Bootstrap.Navbar as Navbar
+
+
+-- You need to keep track of the view state for the navbar in your model
+
+type alias Model =
+    { navbarState : Navbar.State }
+
+
+-- The navbar needs to know the initial window size, so the inital state for a navbar requires a command to be run by the Elm runtime
+
+initialState : (Model, Cmd Msg)
+initialState toMsg =
+    let
+        (navbarState, navbarCmd)
+            = Navbar.initialState NavbarMsg
+    in
+        ({ navbarState = navbarState }, navBarCmd )
+
+
+-- Define a message for the navbar
+
+type Msg
+    = NavbarMsg Navbar.State
+
+
+-- You need to handle navbar messages in your update function to step the navbar state forward
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    Grid.container []
-        [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
-        , Grid.row []
-            [ Grid.col []
-                [ text "Some content for my view here..."]
+    Navbar.config NavbarMsg
+        |> Navbar.withAnimation
+        |> Navbar.brand [ href "#"] [ text "Brand"]
+        |> Navbar.items
+            [ Navbar.itemLink [href "#"] [ text "Item 1"]
+            , Navbar.itemLink [href "#"] [ text "Item 2"]
             ]
+        |> Navbar.view state.basicState
 
-        ]
+-- If you use animations as above or you use dropdowns in your navbar you need to configure subscriptions too
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Navbar.subscriptions model.navbarState NavbarMsg
---
+++ stepper.py
@@ -0,0 +1,37 @@
+from machine import Pin as pin
+p1 = pin(16 , pin.OUT)
+p2 = pin(14 , pin.OUT)
+p3 = pin(12 , pin.OUT)
+p4 = pin(13 , pin.OUT)
+led = pin(15 , pin.OUT)
+
+from time import sleep
+def loop(t=0.01, pin1=p1,pin2=p3,pin3=p2,pin4=p4):
+    '''mind the inversion 1-3-2-4'''
+    while True:
+        pin1.high()
+        pin2.low()
+        pin3.low()
+        pin4.low()
+        sleep(t)
+        pin1.low()
+        pin2.high()
+        pin3.low()
+        pin4.low()
+        sleep(t)
+        pin1.low()
+        pin2.low()
+        pin3.high()
+        pin4.low()
+        sleep(t)
+        pin1.low()
+        pin2.low()
+        pin3.low()
+        pin4.high()
+        sleep(t)
+
+def right(delay):
+    loop(t = delay)
+
+def left(delay):
+    loop(t = delay,pin1=p4,pin2=p2,pin3=p3,pin4=p1)
