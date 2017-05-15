79ac65f007ccad6725b7999bc55b6e0228aedc60
matija <vodokotlic.vudukotlic@gmail.com>
2017-05-15 10:05:48 +0200

    two phase

--- stepper.py
+++ stepper.py
@@ -6,7 +6,7 @@ p4 = pin(13 , pin.OUT)
 led = pin(15 , pin.OUT)
 
 from time import sleep
-def loop(t=0.01, pin1=p1,pin2=p3,pin3=p2,pin4=p4):
+def loop_single(t=0.01, pin1=p1,pin2=p3,pin3=p2,pin4=p4):
     '''mind the inversion 1-3-2-4'''
     while True:
         pin1.high()
@@ -30,8 +30,33 @@ def loop(t=0.01, pin1=p1,pin2=p3,pin3=p2,pin4=p4):
         pin4.high()
         sleep(t)
 
+def loop_double(t=0.01, pin1=p1,pin2=p3,pin3=p2,pin4=p4):
+    '''mind the inversion 1-3-2-4'''
+    while True:
+        pin1.high()
+        pin2.high()
+        pin3.low()
+        pin4.low()
+        sleep(t)
+        pin1.low()
+        pin2.high()
+        pin3.high()
+        pin4.low()
+        sleep(t)
+        pin1.low()
+        pin2.low()
+        pin3.high()
+        pin4.high()
+        sleep(t)
+        pin1.high()
+        pin2.low()
+        pin3.low()
+        pin4.high()
+        sleep(t)
+
+
 def right(delay):
-    loop(t = delay)
+    loop_single(t = delay)
 
 def left(delay):
-    loop(t = delay,pin1=p4,pin2=p2,pin3=p3,pin4=p1)
+    loop_single(t = delay,pin1=p4,pin2=p2,pin3=p3,pin4=p1)
